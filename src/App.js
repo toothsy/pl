754f38b24f6ecf8fdd2e5b421cb76aa26459641d
toothsy <c.j.atharva@gmail.com>
2021-05-04 08:34:23 +0530

    shift to js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <video class="input_video"></video>
+    		<canvas class="output_canvas" width="1280px" height="720px"></canvas>
     </div>
   );
 }
